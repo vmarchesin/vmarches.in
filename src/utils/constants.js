0d46c940ecf6f872fd5ceee5b7250b5e8c5eb469
Vinicius Araujo <vinicius.araujo@arquivei.com.br>
2018-04-04 23:23:59 -0300

    Updates email address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const URLS = {
   facebook: "https://www.facebook.com/vini.marchesin",
   github: "https://github.com/vmarchesin",
   linkedin: "https://www.linkedin.com/in/vinicius-araujo-238a9b96/",
-  mail: "mailto:contact@vmarches.in",
+  mail: "mailto:dev@vmarches.in",
   repo: "https://github.com/vmarchesin/vmarches.in",
   telegram: "https://web.telegram.org/#/im?p=@vmarchesin",
 }
